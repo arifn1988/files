d88e5a672e121caea086d36b71ba4ce6b1725468
arifn1988 <41110831+arifn1988@users.noreply.github.com>
2021-10-08 15:58:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 from os import path
 import zipfile
-
+import pathlib
 
 __winc_id__ = 'ae539110d03e49ea8738fd413ac44ba8'
 __human_name__ = 'files'
+
+dir_path = str(pathlib.Path(__file__).parent.absolute())
 
 def clean_dir(dir):
 	files = os.listdir(dir)
@@ -13,7 +15,7 @@ def clean_dir(dir):
 			os.unlink(os.path.join(dir, file))
 
 def clean_cache():
-	folder_path='C:/Users/gebruiker/Desktop/winc/files/cache'
+	folder_path=dir_path+'/cache'
 	if(path.exists(folder_path)):
 		clean_dir(folder_path)
 	else:
@@ -25,16 +27,16 @@ def cache_zip(zip_file,cache_dir):
 	files= os.listdir(cache_dir)
 
 	if(len(files)==0):
-		file.extractall('C:/Users/gebruiker/Desktop/winc/files/cache')
+		file.extractall(dir_path+'/cache')
 	else:
 		print("Cache directory needs to be empty to extract files to it.")
 
 def cached_files():
-	files= os.listdir('C:/Users/gebruiker/Desktop/winc/files/cache')
+	files= os.listdir('C:/Users/gebruiker/Desktop/winc/files/cache') 
 	absolute_files=[]
 
 	for file in files:
-		absolute_files.append('C:/Users/gebruiker/Desktop/winc/files/cache/'+file) 
+		absolute_files.append(dir_path+'\\cache\\'+file) 
 
 	return absolute_files
 
@@ -44,4 +46,5 @@ def find_password(dir_files):
 
 clean_cache()
 cache_zip('C:/Users/gebruiker/Desktop/winc/files/data.zip','C:/Users/gebruiker/Desktop/winc/files/cache')
-find_password(cached_files())
+#find_password(cached_files())
+
